53df723a1574e62b4a74d56667c131793cf6c506
Leiver Andres Campeon <Leiver Andres Campeon>
2016-11-08 12:27:46 -0500

    add retrieve all users and one user queries

--- users_handler.py
+++ users_handler.py
@@ -1,4 +1,7 @@
 from models.users import User
+import logging
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
 class UsersHandler:
     def __init__(self, DB):
@@ -6,5 +9,26 @@ class UsersHandler:
 
     def create_user(self, user_data):
         collection = self.db.users
-        new_user = User(user_data)
-        collection.insert_one(new_user.__dict__)
+        user = collection.find_one({"username": user_data["username"]})
+        if not user:
+            new_user = User(user_data)
+            collection.insert_one(new_user.__dict__)
+            logging.info("User Created")
+            return True
+        logging.warning("User already exists")
+        return False
+
+    def retrieve_users(self):
+        collection= self.db.users
+        users = collection.find()
+        logging.info("All users retrived successfully")
+        return users
+
+    def retrieve_user(self, username):
+        collection = self.db.users
+        user = collection.find_one({'username': username})
+        if user:
+            logging.info("Users retrived successfully")
+            return user
+        logging.error("User <{}> does not exists".format(username))
+        return None
